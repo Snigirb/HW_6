e1a08b05dc5c606a0740441d09bca4229326cc3d
bogdansnigir <Snigirbog@gmail.com>
2024-09-24 21:14:44 +0500

    second

--- src/Laptop.java
+++ src/Laptop.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+class Laptop {
+    private String brand;
+    private String ram;
+    private String ssd;
 
-public class Laptop {
+    public Laptop (String brand, String ram, String ssd) {
+        this.ram = ram;
+        this.ssd = ssd;
+        this.brand = brand;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getRam() {
+        return ram;
+    }
+
+    public String getSsd() {
+        return ssd;
+    }
+
+    public String toString() {
+        return "Ноутбук - " +
+                "Бренд = " + brand+
+                ", ОЗУ = " + ram +
+                ", Объём ЖД = " + ssd;
+    }
 }
--- src/LaptopFilter.java
+++ src/LaptopFilter.java
@@ -1,6 +1,33 @@
 import java.util.HashSet;
 import java.util.Set;
 
-public class LaptopService {
-    private Set<Laptop> laptops = new HashSet<>();
+
+public class LaptopFilter {
+    public static void filterLaptops(Set<Laptop> laptops, String criteriaKey, String criteriaValue) {
+        Set<Laptop> filteredLaptops = new HashSet<>();
+
+        for (Laptop laptop : laptops) {
+            boolean matches = false;
+
+            switch (criteriaKey.toLowerCase()) {
+                case "1":
+                    matches = laptop.getBrand().equalsIgnoreCase(criteriaValue);
+                    break;
+                case "2":
+                    matches = laptop.getRam().contains(criteriaValue);
+                    break;
+                case "3":
+                    matches = laptop.getSsd().contains(criteriaValue);
+                    break;
+                default:
+                    System.out.println("Неизвестный критерий: " + criteriaKey);
+                    return;
+            }
+
+            if (matches) {
+                filteredLaptops.add(laptop);
+            }
+        }
+        System.out.println("Ноутбуки, подходящие под ваши критерии - "+filteredLaptops);
+    }
 }
